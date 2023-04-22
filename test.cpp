3925bfa83d259e31612b7cc4b833e955f33b4f2c
roma <romana9059@gmail.com>
2023-04-22 14:58:13 +0300

    First commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int n = 0;
+	int del = 0;
+	std::cout << "VVedite n: ";
+	std::cin >> n;
+	for (int i = 1; i < n; i++) {
+		if (n % i == 0) {
+			del = n / i;
+			std::cout << "Diliteli chisla n:  " << del << std::endl;
+		}
+		//else continue;
+	}
+}
